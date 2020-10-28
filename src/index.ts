62e8d17c0d59efc8bdc42f7c4e9e9161985842aa
Tristan Guichaoua <baanlohdev@gmail.com>
2020-10-28 15:00:37 +0100

    📝 add docs

--- src/index.ts
+++ src/index.ts
@@ -22,21 +22,28 @@ export interface Cache<K, V> extends EEI<CacheEvents<K, V>> {}
 
 export class Cache<K, V> implements ReadonlyCache<K, V> {
     private readonly storage = new Map<K, CacheEntry<V>>();
-    private readonly ttl: number;
 
-    constructor(ttl: number) {
-        this.ttl = ttl;
-    }
+    /**
+     * @param ttl Time to live in milliseconds.
+     */
+    constructor(private readonly ttl: number) {}
 
     *[Symbol.iterator](): Iterator<[K, V]> {
         for (const [k, v] of this.storage) yield [k, v.value];
     }
 
+    /** The number of elements in the cache. */
     get size() {
         return this.storage.size;
     }
 
-    set(key: K, value: V, ttl?: number): void {
+    /**
+     * Adds or replaces an element with the specified key.
+     * @param key The key of the element to add.
+     * @param value The value of the element to add.
+     * @param ttl Time to live in milliseconds.
+     */
+    set(key: K, value: V, ttl?: number): this {
         const prev = this.storage.get(key);
         if (prev) clearTimeout(prev.timeout);
         if (!ttl) ttl = this.ttl;
@@ -44,16 +51,31 @@ export class Cache<K, V> implements ReadonlyCache<K, V> {
             value,
             timeout: setTimeout(() => this.delete(key), ttl).unref(),
         });
+        return this;
     }
 
+    /**
+     * Gets an element with the specified key, and returns its value, or `undefined` if the element does not exist.
+     * @param key The key of the element to get.
+     */
     get(key: K): V | undefined {
         return this.storage.get(key)?.value;
     }
 
+    /**
+     * Checks if an element exists in the cache.
+     * @param key The key to check.
+     * @returns Either or not an element exists at the specified key.
+     */
     has(key: K): boolean {
         return this.storage.has(key);
     }
 
+    /**
+     * Deletes an element from the cache.
+     * @param key The key to delete from the cache.
+     * @returns Either or not the element was removed.
+     */
     delete(key: K): boolean {
         const entry = this.storage.get(key);
         if (entry) {
@@ -65,6 +87,9 @@ export class Cache<K, V> implements ReadonlyCache<K, V> {
         return false;
     }
 
+    /**
+     * Removes all elements from the cache.
+     */
     clear(): void {
         for (const entry of this.storage.values()) clearTimeout(entry.timeout);
         this.storage.clear();
