57c580994a47ccbabc4c0cccd88706434ab7daa9
Tristan Guichaoua <baanlohdev@gmail.com>
2020-10-29 14:08:23 +0100

    ✨ restart ttl

--- src/index.ts
+++ src/index.ts
@@ -58,8 +58,30 @@ export class Cache<K, V> implements ReadonlyCache<K, V> {
      * Gets an element with the specified key, and returns its value, or `undefined` if the element does not exist.
      * @param key The key of the element to get.
      */
-    get(key: K): V | undefined {
-        return this.storage.get(key)?.value;
+    get(key: K): V | undefined;
+    /**
+     * Gets an element with the specified key, and returns its value, or `undefined` if the element does not exist.
+     * If restart is `true`, the ttl of the element (if it exists) is reset to the defautl ttl.
+     * @param key The key of the element to get.
+     * @param restart Either or not the ttl is reset to the default ttl.
+     */
+    get(key: K, restart: boolean): V | undefined;
+    /**
+     * Gets an element with the specified key, and returns its value, or `undefined` if the element does not exist.
+     * The ttl of the element (if it exists) is reset to the restartTtl.
+     * @param key The key of the element to get.
+     * @param restartTtl The time to live in milliseconds to set.
+     */
+    get(key: K, restartTtl: number): V | undefined;
+    get(key: K, restart?: boolean | number): V | undefined {
+        const e = this.storage.get(key);
+        if (!e) return undefined;
+        if (typeof restart !== 'undefined' && restart !== false) {
+            const ttl = typeof restart === 'number' ? restart : this.ttl;
+            clearTimeout(e.timeout);
+            e.timeout = setTimeout(() => this.delete(key), ttl).unref();
+        }
+        return e.value;
     }
 
     /**
@@ -93,6 +115,16 @@ export class Cache<K, V> implements ReadonlyCache<K, V> {
     clear(): void {
         for (const entry of this.storage.values()) clearTimeout(entry.timeout);
         this.storage.clear();
+    }
+
+    /**
+     * Resets the ttl of the element (if it exists) to the ttl value.
+     * @param key The key of the element to restart.
+     * @param ttl The time to live in milliseconds to set or the default ttl.
+     * @returns Either or not the element exists.
+     */
+    restart(key: K, ttl: number = this.ttl): boolean {
+        return this.get(key, ttl) !== undefined;
     }
 }
 
